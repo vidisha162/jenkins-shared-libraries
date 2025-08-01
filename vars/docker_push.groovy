acf8d5e25d6f83c0c683521bde95c4b9b56ce3eb
vidisha <vidishafirani66@gmail.com>
2025-08-01 21:23:47 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,9 @@
+def call(String Project, String Image, String dockerhubuser){
+   withCredentials([usernamePassword
+        ('credentialsId':"dockerHubCred",
+        passwordVariable:"dockerHubPass",
+        usernameVariable:"dockerHubUser")]){
+        bat "docker login -u %dockerHubUser% -p %dockerHubPass%"
+        bat "docker tag vaultify-backend %dockerHubUser%/vaultify-backend"
+        bat "docker push %dockerHubUser%/vaultify-backend"
+}
